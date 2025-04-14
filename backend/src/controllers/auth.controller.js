1281cb2f8e33d786bba91c504595c510e7285be2
Ajay KC <meajay64@gmail.com>
2025-04-14 16:31:21 +0545

    Add auth routers with middlewares

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,11 @@
+export const signup = () => {
+  res.send("signup route");
+};
+
+export const login = () => {
+  res.send("login route");
+};
+
+export const logout = () => {
+  res.send("logout route");
+};
